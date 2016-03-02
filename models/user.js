d15926d160e8effd00459b672be46f0ec7a39f4d
Sharon Rumsey <sharon@rumsey.nz>
2016-03-03 11:46:13 +1300

    faker data and connect to mongo

---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+	modules.exports = mongoose.Schema({
+		email: String,
+		firstName: String, 
+		lastName: String,
+		passwordHash: String
+		age: Number,
+		description: String,
+		location: String
+		img: String
+	})
