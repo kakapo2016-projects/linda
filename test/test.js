ebf66f53f980e968ad4c2febad673ba408a496af
Jason Veng <email": null>
2016-03-04 11:21:42 +1300

    push online

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
-var dbURI = 'mongodb://localhost:27017/linda'
 var should = require('chai').should()
 var mongoose = require('mongoose')
 var clearDB  = require('mocha-mongoose')(dbURI)
-var dummyTest = mongoose.model('Dummy', new mongoose.Schema({
+
+var dbURI = 'mongodb://localhost:27017/linda'
+
+var Dummy = mongoose.model('Dummy', new mongoose.Schema({
   email: String,
   firstName: String,
   lastName: String,
@@ -12,65 +14,3 @@ var dummyTest = mongoose.model('Dummy', new mongoose.Schema({
   location: String,
   img: String
 }))
-
-describe('testing connection to db', function() {
-  beforeEach(function(done) {
-    if (mongoose.connection.db) return done();
-    mongoose.connect(dbURI, done)
-  })
-
-  it('insert() user in the db', function(done) {
-    new dummyTest({
-      email: String,
-      firstName: String,
-      lastName: String,
-      passwordHash: String,
-      age: String,
-      description: String,
-      location: String,
-      img: String
-    }).insert(dummyTest)
-  })
-  it('find() users in the db', function(done) {
-    new dummyTest({
-      email: String,
-      firstName: String,
-      lastName: String,
-      passwordHash: String,
-      age: String,
-      description: String,
-      location: String,
-      img: String
-    }).save(done)
-  })
-})
-
-describe('testing to see if user can sign in', function() {
-  it('', function(done) {
-    new dummyTest({
-      email: String,
-      firstName: String,
-      lastName: String,
-      passwordHash: String,
-      age: String,
-      description: String,
-      location: String,
-      img: String
-    })
-  })
-})
-
-describe('testing to see if user can sign up', function() {
-  it('', function(done) {
-    new dummyTest({
-      email: String,
-      firstName: String,
-      lastName: String,
-      passwordHash: String,
-      age: String,
-      description: String,
-      location: String,
-      img: String
-    })
-  })
-})
