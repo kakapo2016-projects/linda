6c8793e031909a68fc0f92d4d2a1e1a058d2d003
Jason Veng <email": null>
2016-03-04 10:01:37 +1300

    convert tab to space

--- db/seeddb.js
+++ db/seeddb.js
@@ -8,47 +8,41 @@ var Faker = require('faker')
 // fakeUserOptions (n) =>  [{}, {}]
 function fakerUsers(userNumber) {
 
-	var userOptions = []
-	for (var i = 0; i < userNumber; i++) {
-  	var user = {
-	   	email: Faker.Internet.email(),
-	   	firstName: Faker.Name.firstName(),
-	   	lastName: Faker.Name.lastName(),
-	   	age: 32,
-	   	description: "Pretend description!!",
-	   	location: Faker.Address.city(),
-	   	img: Faker.Image.imageUrl()
-   	}
-   	userOptions.push(user)
- 	}
- 	return userOptions
+  var userOptions = []
+  for (var i = 0; i < userNumber; i++) {
+    var user = {
+      email: Faker.Internet.email(),
+      firstName: Faker.Name.firstName(),
+      lastName: Faker.Name.lastName(),
+      age: 32,
+      description: "Pretend description!!",
+      location: Faker.Address.city(),
+      img: Faker.Image.imageUrl()
+    }
+    userOptions.push(user)
+  }
+  return userOptions
 }
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 
-	var userModel = mongoose.model('User', userSchema)
-	console.log("DATABASE OPEN!")
-	var users = fakerUsers(20) 
-	users.forEach(function (user) {
-		var userForDB = new userModel(user)
-		// userForDB.save(function (err, userForDB) {
-		// 	if (err) return console.error(err)
-		// 	console.log(userForDB.firstName, "saved sucessfully! (maybe...)")
-		// })
-		userModel.find(function (err, userFromDB) {
-  	if (err) return console.error(err);
-  	console.log(userFromDB);
-})
-	})
+  var userModel = mongoose.model('User', userSchema)
+  console.log("DATABASE OPEN!")
+  var users = fakerUsers(20)
+  users.forEach(function (user) {
+    var userForDB = new userModel(user)
+    // userForDB.save(function (err, userForDB) {
+    //   if (err) return console.error(err)
+    //   console.log(userForDB.firstName, "saved sucessfully! (maybe...)")
+    // })
+    userModel.find(function (err, userFromDB) {
+      if (err) return console.error(err);
+      console.log(userFromDB);
+    })
+  })
 
-	// fakeUsers.save(function (err, fakerUsers) {
-	// 	if (err ) return console.log(err);
-
-	// })
+// fakeUsers.save(function (err, fakerUsers) {
+//   if (err ) return console.log(err);
 });
-
-
-
-
