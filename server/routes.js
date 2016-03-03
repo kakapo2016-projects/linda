7206e313b18bd6bdf7636da0bfd0c2618e99bf37
Sharon Rumsey <sharon@rumsey.nz>
2016-03-04 12:10:52 +1300

    add profile get

--- server/routes.js
+++ server/routes.js
@@ -9,9 +9,28 @@ exports = module.exports = function (app) {
 
   app.post('/sign-up', function (req, res) {
     getUsers(function (err, users) {
-    	if (err) console.log(err)
-
-    		var newEmail = ''
+    	// set internal db variable
+    	var db = req.db
+    	// check for email already in db
+    	if (users.email === req.body.email) {
+    		res.redirect('/profile')
+    		console.log('already in the db', req.body.email)
+    	}
+    		// get our form values based upon name attributes
+    		console.log('>>>>>>',res.body.email)
+    		var newUserEmail = req.body.email;
+    		// set the collection
+    		var collection = db.get(getUsers)
+    		// submit to db
+    		collection.insert({
+    			"email": newUserEmail
+    		}, function (err, doc) {
+    			if (err) {
+    				res.send("There was a problem you are not a Linda prescriber")
+    			} else {
+    				res.redirect('/profile')
+    			}
+    		})
     		// extract new users email address from req 
 
     		var userArray = Array.from(users)
@@ -27,6 +46,9 @@ exports = module.exports = function (app) {
 
   app.get('/sign-up', function (req, res) {
     res.render('sign-up')
+  })
+  app.get('/profile', function (req, res) {
+    res.render('profile')
   })
 
   app.post('/sign-in', function (req, res) {
