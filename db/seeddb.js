a612bce2a65bbd70c91ec407dda868c58e955cfb
Sharon Rumsey <sharon@rumsey.nz>
2016-03-04 10:35:43 +1300

    connecting but a problem

--- db/seeddb.js
+++ db/seeddb.js
@@ -1,54 +1,76 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/linda');
+// mongoose.connect('mongodb://localhost:27017/linda');
 
 var userSchema = require('../models/user')
 var Faker = require('faker')
 
 
 // fakeUserOptions (n) =>  [{}, {}]
-function fakerUsers(userNumber) {
+// function fakerUsers(userNumber) {
 
-	var userOptions = []
-	for (var i = 0; i < userNumber; i++) {
-  	var user = {
-	   	email: Faker.Internet.email(),
-	   	firstName: Faker.Name.firstName(),
-	   	lastName: Faker.Name.lastName(),
-	   	age: 32,
-	   	description: "Pretend description!!",
-	   	location: Faker.Address.city(),
-	   	img: Faker.Image.imageUrl()
-   	}
-   	userOptions.push(user)
- 	}
- 	return userOptions
-}
+// 	var userOptions = []
+// 	for (var i = 0; i < userNumber; i++) {
+//   	var user = {
+// 	   	email: Faker.Internet.email(),
+// 	   	firstName: Faker.Name.firstName(),
+// 	   	lastName: Faker.Name.lastName(),
+// 	   	age: 32,
+// 	   	description: "Pretend description!!",
+// 	   	location: Faker.Address.city(),
+// 	   	img: Faker.Image.imageUrl()
+//    	}
+//    	userOptions.push(user)
+//  	}
+//  	return userOptions
+// }
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
 
-	var userModel = mongoose.model('User', userSchema)
-	console.log("DATABASE OPEN!")
-	var users = fakerUsers(20) 
-	users.forEach(function (user) {
-		var userForDB = new userModel(user)
+// 	var userModel = mongoose.model('User', userSchema)
+// 	console.log("DATABASE OPEN!")
+// 	var users = fakerUsers(20) 
+// 	users.forEach(function (user) {
+// 		var userForDB = new userModel(user)
 		// userForDB.save(function (err, userForDB) {
 		// 	if (err) return console.error(err)
 		// 	console.log(userForDB.firstName, "saved sucessfully! (maybe...)")
 		// })
-		userModel.find(function (err, userFromDB) {
-  	if (err) return console.error(err);
-  	console.log(userFromDB);
-})
-	})
+
+		// userModel.find(function (err, userFromDB) {
+  // 	if (err) return console.error(err);
+  	// console.log(userFromDB);
+// })
+// 	})
 
 	// fakeUsers.save(function (err, fakerUsers) {
 	// 	if (err ) return console.log(err);
 
 	// })
-});
+// });
+// var userModel = mongoose.model('User', userSchema)
 
+// 		users.forEach(function (user) {
+// 		var userForDB = new userModel(user)
+// 		userModel.find(function (err, userFromDB) {
+//   	if (err) return console.error(err);
+  	// console.log(userFromDB);
+// })
+// })
 
-
+module.exports = function getUsers(callback) {
+	// console.log('Im alive')
+	mongoose.connect('mongodb://localhost:27017/linda');
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function() {
+		// console.log('im open')
+	var userModel = mongoose.model('User', userSchema)
+	userModel.find(function (err, res) {
+		// console.log('oh res', res)
+		callback(err, res)
+	})
+})
+}
 
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,17 @@
 'use strict'
+var getUsers = require('../db/seeddb.js')
 
 exports = module.exports = function (app) {
   app.get('/', function (req, res) {
     res.render('home')
+  })
+
+
+  app.get('/test', function (req, res) {
+    getUsers(function (err, res) {
+    	if (err) console.log(err)
+    		console.log('test response', res)
+    })
   })
 
   app.get('/sign-up', function (req, res) {
