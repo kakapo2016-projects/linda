c3f398a96297224560570497725fadfe28da416a
Jason Veng <email": null>
2016-03-04 14:40:47 +1300

    merge master

--- server/routes.js
+++ server/routes.js
@@ -9,19 +9,19 @@ exports = module.exports = function (app) {
 
   app.post('/sign-up', function (req, res) {
     getUsers(function (err, users) {
-    	if (err) console.log(err)
+      if (err) console.log(err)
 
-    		var newEmail = ''
-    		// extract new users email address from req 
+        var newEmail = ''
+        // extract new users email address from req
 
-    		var userArray = Array.from(users)
-    		userArray.forEach(function(user) {
-    			if (user.email === newEmail) console.log(user)
-    			// if the users email already exists in the server, they are a LOSER
-    		})
+        var userArray = Array.from(users)
+        userArray.forEach(function(user) {
+          if (user.email === newEmail) console.log(user)
+          // if the users email already exists in the server, they are a LOSER
+        })
 
-    			// Otherwise, we're going to add them to the database
-    			// And redirect them to the (/profile) page
+          // Otherwise, we're going to add them to the database
+          // And redirect them to the (/profile) page
     })
   })
 
