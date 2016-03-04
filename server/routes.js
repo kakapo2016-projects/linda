04254233007fadfa073fba0928d70a7f23de616b
Sharon Rumsey <sharon@rumsey.nz>
2016-03-04 15:23:22 +1300

    displaying profile

--- server/routes.js
+++ server/routes.js
@@ -42,12 +42,31 @@ app.post('/sign-up', function (req, res) {
     res.render('sign-up')
   })
   app.get('/profile', function (req, res) {
-  	// var profile = req.body;
-   //  console.log('profile', profile)
-   //  res.render('profile', {"user": profile})
   })
 
   app.post('/sign-in', function (req, res) {
-    res.send("trying to sign in, are you?")
-  })
+    var userExists = false
+	var db = mongoose.connect('mongodb://localhost:27017/linda');
+    dbFunctions.getUsers(db, function (err, users) {
+		var newEmail = req.body.email;
+    		var userArray = Array.from(users)
+    		userArray.forEach(function(user) {
+    			if (user.email === newEmail) {
+    			userExists = true
+    			db.disconnect(function() {
+    			console.log('database closed')
+    			res.render('profile', {"user": user})
+    		})
+    	}
+    	
+    	})
+  	if (!userExists) {
+				console.log(req.body.email, "User doen't exist yet!")
+    		db.disconnect(function() {
+    			console.log('database closed')
+	    		res.render('sign-up')
+    		})
+	  	}
+	  })
+	})
 }
